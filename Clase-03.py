96e5bdbb7f1948f7a0a6b6ae1395384e16761211
Leonardoih <leonardoih090217@gmail.com>
2025-01-05 16:27:28 -0500

    Agregar comentarios y mejorar la estructura en Clase-03.py

--- Clase-03.py
+++ Clase-03.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+#cambiar el tamaño de la ventana
 root=Tk()
 root.title("Posicionar")
 root.geometry("400x200")
@@ -10,14 +11,17 @@ def saludo():
 def minimizar():
     root.iconify()
 
+#create label
 etiqueta = Label(root, text="Saluda desde aqui")
 etiqueta.place(x=30, y=50)
 
 etiqueta2 = Label(root, text="Minimiza desde aqui")
 etiqueta2.place(x=30, y=100)
 
+#crear botones
 boton1= Button(root, text="Saludar", command=saludo)
 boton1.place(x=200, y=50)
+
 
 boton2= Button(root, text="Minimizar", command=minimizar)
 boton2.place(x=200, y=100)
