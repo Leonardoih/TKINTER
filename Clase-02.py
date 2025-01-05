4aa6841b5c785710f3240d8bc32b72e1d18b7a74
Leonardoih <leonardoih090217@gmail.com>
2025-01-05 16:25:24 -0500

    agregando comentarios en en el archivo Clase-02.py

--- Clase-02.py
+++ Clase-02.py
@@ -1,13 +1,17 @@
 from tkinter import *
 import time
+#Importar libreria de tiempo
 
+#Crear ventana
 root = Tk()
 root.title = ("Python")
 root.geometry("500x400")
 
+#Crear boton 1
 boton1 = Button(root, text="Minimizar", command=root.iconify, bg="green")
 boton1.pack(side=LEFT)
 
+#Crear boton 2
 def imprimir():
     label1 = Label(root, text="Imprimiendo...")
     label1.pack()
